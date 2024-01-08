403155484b42c958539e2149d2e9350641e1ef8c
kamil.klimaszewski@gmail.com <kamil.klimaszewski@gmail.com>
2024-01-08 22:22:53 +0100

    Libraries updated and Java 11. Version 2024.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        Info info = new Info("Kamil", "1.0.0", "2022");
+        Info info = new Info("Kamil", "1.0.0", "2024");
         System.out.println(info.toJson());
     }
 }
